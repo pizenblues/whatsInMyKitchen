f4f553078f0dc6691f7bf142b1783c3ee985c4b4
zookid <pizenblues@gmail.com>
2019-01-13 18:38:20 -0400

    inicializando aplicacion

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        ingridients: [
+            { name: 'Queso' },
+            { name: 'Pan' },
+            { name: 'Papas' }
+          ]
+    }
+  })
